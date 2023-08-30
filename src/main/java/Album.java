fcdfafb36e5a48c0b1e502dfdd435cf36f2e7203
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-30 09:47:14 -0500

    making beans

---
+++ src/main/java/Album.java
@@ -0,0 +1,68 @@
+import java.util.Date;
+
+public class Album {
+    private long id;
+    private String artist;
+    private String name;
+    private int releaseDate;
+    private double sales;
+    private String genre;
+
+    public Album( String artist, String name, int releaseDate, double sales, String genre) {
+        this.artist = artist;
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.sales = sales;
+        this.genre = genre;
+    }
+    public Album() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
---
+++ src/main/java/beanTest.java
@@ -0,0 +1,9 @@
+public class beanTest {
+    public static void main(String[] args) {
+        Album album = new Album("Limp Bizkit", "Break Stuff", 1999, 25, "Rock");
+        Album emptyalbum = new Album();
+        Author author = new Author("employees", "Mustwashhands");
+        Quote quote = new Quote("its just one of those days", author);
+
+    }
+}
