234693e94ba973d7ef3669557b70c7cde98d7bb1
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-31 11:54:19 -0500

    java database connection

---
+++ src/main/java/JDBCEmployees.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class JDBCEmployees {
+    public static void main(String[] args) throws SQLException {
+
+        DriverManager.registerDriver(new Driver());
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/employees?allowPublicKeyRetrieval=true&useSSL=false",
+                "root",
+                "codeup"
+        );
+        Statement statement = connection.createStatement();
+//        statement.executeQuery("use employees");
+//        statement.executeQuery("select * employees");
+        ResultSet rs = statement.executeQuery("select * from employees limit 10");
+        ArrayList<String> names = new ArrayList<>();
+        while (rs.next()){
+            names.add(rs.getString("first_name"));
+        }
+        System.out.println(names);
+
+    }
+}
